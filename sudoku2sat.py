7636ed667de45ecb8ed6a03f77e6d46884ce9ded
Peter Zenger <peter.zenger1@gmail.com>
2013-01-27 20:37:09 -0800

    Almost have it working

--- sudoku2sat.py
+++ sudoku2sat.py
@@ -5,8 +5,6 @@
 from __future__ import print_function
 import sys, math
 
-#numbers = [1,2,3,4,5,6,7,8,9]
-
 def load_board(name):
     """Loads a board which is defined by a 9x9 grid in a file"""
     try:
@@ -16,57 +14,106 @@ def load_board(name):
         #If the board is inputed with spaces between numbers (for general form)
         if ' ' in data[0]:
             print("Format 2")
-            board_2d = [[int(x) for x in line.split(' ')] for line in data]
+            board_2d = [[x for x in line.split(' ')] for line in data]
         else:
             print("Format 1")
-            board_2d = [[int(x) for x in line.strip()] for line in data] 
+            board_2d = [[x for x in line.strip()] for line in data] 
         f.close()
         return board_2d
     except IOError:
         print("Error opening '%s', check spelling and try again" % name)
         sys.exit(-1)
 
-
-def row(board, x):
-    """Returns all integers of a row"""
-    return [int(board[x][i]) for i in xrange(9)]
-
-def col(board, x):
-    """returns all integers of a column"""
-    return [int(board[i][x]) for i in xrange(9)]
-
-def square(b, x, y = -1):
-    """Returns the integer values in a 3x3 square
-    Top left is 0, top right is 2, bottom left is 6 etc"""
-    if y >= 0:
-        if x < 3:
-            x = int(y/3)
-        elif x < 6:
-            x = 3 + int(y/3)
-        else:
-            x = int(y/3)+ 6
-    row = int(x/3)*3
-    col = (x % 3)*3
-    return [int(b[row+i][col+j]) for i in xrange(3) for j in xrange(3)]
-
-def candidates(board, r, c):
-    """Returns all of the possible numbers for a specific cell"""
-    if int(board[r][c]) == 0:
-        return [i for i in numbers if i not in row(board, r) and i not in col(board, c) and i not in square(board, r, c)]
-    else:
-        return []
-
-def all_candidates(board):
-    """Returned all the candidate numbers for every spot which does not yet have a value"""
-    possible = [(candidates(board, x, y), x, y) for x in xrange(9) for y in xrange(9)]
-    filtered = [x for x in possible if len(x[0]) > 0]
-    sorted_possible = sorted(filtered, lambda x,y: 1 if len(x[0]) > len(y[0]) else -1 if len(y[0]) > len(x[0]) else 0)
-    return sorted_possible
-
 def stringify_board(board):
     """ Turn the board into a string for outputting """
     return ''.join(str(item) for line in board for item in line)
 
+def generate_row_pairs(n):
+    """Generates a list of lists, 
+    Where each of the inner lists is a clause which restricts the values within a row.
+    The overall list contains all the clauses which allow a value to
+    only appear once in any row."""
+    res = []
+
+    for r in xrange(n): #Row
+        for c in xrange(n): #Column
+            for v in xrange(n): #Value
+                for o in range(c+1,n): #Other cell
+                    lhs = (r*n**2)+c*n+v+1
+                    rhs = (r*n**2)+o*n+v+1
+
+                    clause = [lhs*-1, rhs*-1]
+                    res.append(clause)
+
+    return res
+
+def generate_column_pairs(n):
+    """Generates a list of lists, 
+    Where each of the inner lists is a clause which restricts the values within a column.
+    The overall list contains all the clauses which allow a value to
+    only appear once in any column."""
+    res = []
+
+    for c in xrange(n): #Column
+        for r in xrange(n): #Row
+            for v in xrange(n): #Value
+                for o in range(r+1, n): #Other cell
+                    lhs = (r*n**2)+c*n+v+1
+                    rhs = o*n**2+c*n+v+1
+
+                    clause = [lhs*-1, rhs*-1]
+                    res.append(clause)
+    return res
+
+def generate_square_pairs(n):
+    """ Generates a list of lists,
+    Where the inner lists are the atoms for the clauses which restrict
+    values within the subsquares"""
+
+    s = int(math.sqrt(n))
+
+    res = []
+
+    for x in xrange(s): #for each square in a row
+        for k in xrange(s): #for each square in a column
+            for i in xrange(s): #row within square
+                for j in xrange(s): #column within square
+                    for v in xrange(n): #value of cell
+                        for oi in xrange(s): #other cells row
+                            for oj in xrange(s): #other cells column
+                                if oi <= i and oj <= j:
+                                    pass
+                                else:
+                                    square_value = (x*n*n*s) + (n*s*k)
+                                    lhs = square_value + (i*n*n) + (j*n) + v + 1
+                                    rhs = square_value + (oi*n*n) + (oj*n) + v + 1
+
+                                    clause = [lhs*-1, rhs*-1]
+                                    res.append(clause)
+    return res
+
+def generate_unit_clauses(board):
+    """Find the initial unit clauses
+    Based on what is already filled in on the input board"""
+
+    res = []
+
+    n = len(board)
+    for i in xrange(n):
+        for j in xrange(n):
+            if board[i][j].isdigit():
+                print ("b",board[i][j])
+    return res
+
+def format_clauses(clauses):
+    """ Format clauses for desired output
+    In this case, add a 0 to the end of each one"""
+
+    fixed_clauses = [x+[0] for x in clauses]
+    return fixed_clauses
+
+def create_report(clauses, out_filename):
+    pass
 
 def main():
     if len(sys.argv) < 3 or len(sys.argv) > 3:
@@ -75,7 +122,7 @@ def main():
         sys.exit(-1)
 
     input_board = sys.argv[1]
-    output_board = sys.argv[2]
+    output_filename = sys.argv[2]
 
     board = load_board(input_board)
     m = int(math.sqrt(len(board[0])))   #Size of each subsquare
@@ -86,35 +133,31 @@ def main():
 
     #List of lists for all holds all the atoms for each cell
     #eg. [[1,2...9],[10,11...18], ... [721,722...729]]
-    squares_possible_colours = [[x+(y*n)+1 for x in xrange(n)] for y in xrange(cells)]
+    ensure_colour_clauses = [[x+(y*n)+1 for x in xrange(n)] for y in xrange(cells)]
+    row_clauses = generate_row_pairs(n)
+    column_clauses = generate_column_pairs(n)
+    square_clauses = generate_square_pairs(n)
+    unit_clauses = generate_unit_clauses(board)
 
-    print(squares_possible_colours)
+    all_clauses = ensure_colour_clauses + row_clauses + column_clauses + square_clauses
 
-    #squares_have_colours = [x+y for x in [1,2,3] for y in [3,4,5]] 
 
-    #Generate Formulas for Each square has value (C111 or C112 or ... or C11N)
-    #Number % sqr(m) == n value
-    #Generate Forumulas for each row
-    #Generate forumulas for each column
-    #Generate formulas for each square
+    #print(ensure_colour_clauses)
+    #print(square_clauses)
+    #print(len(all_clauses))
+    print("%s" % "--"*50)
+    
+
+    all_clauses = format_clauses(all_clauses)
+    #print(all_clauses)
+
+    create_report(all_clauses, output_filename)
+
+
     #Parse board to add unit clauses
-    #Joing it all together
+    #report
 
-    """c_list = all_candidates(board)
-    while len(c_list) > 0:
-        r = c_list[0][1]
-        c = c_list[0][2]
-        value = c_list[0][0][0]
-        board[r][c] = value
-        c_list = all_candidates(board)
    
-    output_board = stringify_board(board)
-
-    print "Completed board: "
-    print output_board
-    with open(file_name+".sol", 'w') as f:
-        f.write(output_board)"""
-
     
 if __name__ == "__main__":
     main()
