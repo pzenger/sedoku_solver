f99c89297648a652e5aefe6ed2a3253bc74bfc25
pzenger <peter.zenger1@gmail.com>
2013-04-10 20:20:55 -0700

    things

--- direct/run_all.py
+++ direct/run_all.py
@@ -8,39 +8,39 @@ import os
 
 
 # 9 with slow
-os.system('python timing.py solver_1.py generated/size9/20%')
-os.system('python timing.py solver_1.py generated/size9/40%')
+#os.system('python timing.py solver_1.py generated/size9/20%')
+#os.system('python timing.py solver_1.py generated/size9/40%')
 
 
 # 9 and 16 with med solver
-os.system('python timing.py solver_2.py generated/size9/20%')
-os.system('python timing.py solver_2.py generated/size9/40%')
-os.system('python timing.py solver_2.py generated/size16/20%')
-os.system('python timing.py solver_2.py generated/size16/40%')
+#os.system('python timing.py solver_2.py generated/size9/20%')
+#os.system('python timing.py solver_2.py generated/size9/40%')
+#os.system('python timing.py solver_2.py generated/size16/20%')
+#os.system('python timing.py solver_2.py generated/size16/40%')
 
 # 9 and 16 with fast solver
-os.system('python timing.py solver3_.py generated/size9/20%')
-os.system('python timing.py solver_3.py generated/size9/40%')
-os.system('python timing.py solver_3.py generated/size16/20%')
-os.system('python timing.py solver_3.py generated/size16/40%')
+#os.system('python timing.py solver_3.py generated/size9/20%')
+#os.system('python timing.py solver_3.py generated/size9/40%')
+#os.system('python timing.py solver_3.py generated/size16/20%')
+#os.system('python timing.py solver_3.py generated/size16/40%')
 
 
 # 25 with fastest 2 solvers, only 40% with fastest
 os.system('python timing.py solver_2.py generated/size25/20%')
 os.system('python timing.py solver_3.py generated/size25/20%')
-os.system('python timing.py solver_3.py generated/size25/40%')
+#os.system('python timing.py solver_3.py generated/size25/40%')
 
 # a 36 with fastest
 os.system('python timing.py solver_3.py generated/size36/20%')
-os.system('python timing.py solver_3.py generated/size36/40%')
+#os.system('python timing.py solver_3.py generated/size36/40%')
 
 # 16 with slow
 os.system('python timing.py solver_1.py generated/size16/20%')
-os.system('python timing.py solver_1.py generated/size16/40%')
+#os.system('python timing.py solver_1.py generated/size16/40%')
 
 # Just incase it manages to get this far:
 os.system('python timing.py solver_2.py generated/size36/20%')
-os.system('python timing.py solver_2.py generated/size36/40%')
+#os.system('python timing.py solver_2.py generated/size36/40%')
 
 
 
